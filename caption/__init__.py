fe6776d744edc3a92eda1a9604cd73d2b775a02c
Helmut Konrad Schewe <helmutus@outlook.com>
2026-07-23 14:21:19 +0200

    chore(pyproject): add modern project layout

--- caption/__init__.py
+++ caption/__init__.py
@@ -7,9 +7,10 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
+import importlib.metadata
 import os
 
-__version__ = '0.4.6'
+PROCESS = 'caption'
+__version__ = importlib.metadata.version(PROCESS)
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-PROCESS = 'caption'
